b99265b2d73c2c51b4dcb34e8fe3ab4d7404fd1d
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-24 20:43:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 import Calculator from './components/calculator';
+import RandomQuotes from './components/qoutes';
 
 function App() {
   return (
     <div className="App">
+      <RandomQuotes />
       <Calculator />
     </div>
   );
