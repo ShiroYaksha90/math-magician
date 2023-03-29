4ee0f88881e3831218cfed6a193469e7db467449
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-03-30 01:36:07 +0300

    Add Navigation.js file

---
+++ src/components/Navigation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './calculator.css';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => (
+  <div>
+    <nav className="nav">
+      <a href="/" className="title">
+        Math Magician
+      </a>
+      <ul>
+        <li>
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/Calculator" className="nav-link">
+            Calculator
+          </Link>
+        </li>
+        <li>
+          <Link to="/Quote" className="nav-link">
+            Quote
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Navigation;
